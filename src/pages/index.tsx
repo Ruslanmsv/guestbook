f96137aa65cb2dab9feb3ea1ce86b893df69f161
Ruslan Musaev <ruslan.msv@gmail.com>
2023-04-24 00:50:23 +0200

    Added github login, updated packagess

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -112,10 +112,10 @@ const Home = () => {
             type="button"
             className="mx-auto block rounded-md bg-neutral-800 py-3 px-6 text-center hover:bg-neutral-700"
             onClick={() => {
-              signIn("discord").catch(console.log);
+              signIn().catch(console.log);
             }}
           >
-            Login with Discord
+            Login
           </button>
         )}
         <div className="pt-10">
